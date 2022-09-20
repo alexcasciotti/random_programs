a8632d1eb7318208799c088563de975c288e24d8
Alex Casciotti <alexc2048@gmail.com>
2022-09-19 21:24:59 -0400

    updated calc.py

--- calc.py
+++ calc.py
@@ -1,42 +1,29 @@
-expression = input('Enter a mathematical expression...\nValid operators:\n(+)\n(-)\n(*)\n(/)\n(**)\n')
-for x in expression:
-    if(x == '+'):
-        index = expression.index(x)
-        try:
-            first_num = float(expression[0:index])
-            second_num = float(expression[index+1:])
-            print(first_num + second_num)
-        except ValueError:
-            print('Enter numbers for the operands...')
-    if(x == '-'):
-        index = expression.index(x)
-        try:
-            first_num = float(expression[0:index])
-            second_num = float(expression[index+1:])
-            print(first_num - second_num)
-        except ValueError:
-            print('Enter numbers for the operands...')
-    if(x == '*'):
-        index = expression.index(x)
-        try:
-            first_num = float(expression[0:index])
-            second_num = float(expression[index+1:])
-            print(first_num * second_num)
-        except ValueError:
-            print('Enter numbers for the operands...')
-    if(x == '/'):
-        index = expression.index(x)
-        try:
-            first_num = float(expression[0:index])
-            second_num = float(expression[index+1:])
-            print(first_num / second_num)
-        except ValueError:
-            print('Enter numbers for the operands...')
-    if(x == '**'):
-        index = expression.index(x)
-        try:
-            first_num = float(expression[0:index])
-            second_num = float(expression[index+1:])
-            print(first_num ** second_num)
-        except ValueError:
-            print('Enter numbers for the operands...')
+import data_structures as ds
+
+def calc(x, y, op):
+    if(op == '+'):
+        return x + y
+    if(op == '-'):
+        return x - y
+    if(op == '*'):
+        return x * y
+    if(op == '/'):
+        return x / y
+    if(op == '**'):
+        return x ** y
+        
+exit = 0
+saved_outputs = ds.myStack()
+while(exit != 1):
+    try:
+        x = float(input('X : '))
+        y = float(input('Y : '))
+        op = input('Operation?\n(+)\n(-)\n(*)\n(/)\n(**)\n')
+        result = calc(x, y, op)
+        print(f'{x} {op} {y} = {result}')
+        exit = input('Exit?(Y or N)\n')
+        if(exit.upper() == 'Y'):
+            exit = 1
+    except ValueError:
+        print('Operands must be decimal or integer values.')
+
