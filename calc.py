1d2f3348559def39d75d193f84842de798147873
Alex Casciotti <alexc2048@gmail.com>
2022-09-19 20:58:55 -0400

    calc.py update

--- calc.py
+++ calc.py
@@ -1,47 +1,42 @@
-
-# smaple input
-# 3949 + 9339
-def calc():
-    expression = input('Enter a mathematical expression...\nValid operators:\n(+)\n(-)\n(*)\n(/)\n(**)\n')
-    for x in expression:
-        if(x == '+'):
-            index = expression.index(x)
-            try:
-                first_num = float(expression[0:index])
-                second_num = float(expression[index+1:])
-                return first_num + second_num
-            except ValueError:
-                print('Enter numbers for the operands...')
-        if(x == '-'):
-            index = expression.index(x)
-            try:
-                first_num = float(expression[0:index])
-                second_num = float(expression[index+1:])
-                return first_num - second_num
-            except ValueError:
-                print('Enter numbers for the operands...')
-        if(x == '*'):
-            index = expression.index(x)
-            try:
-                first_num = float(expression[0:index])
-                second_num = float(expression[index+1:])
-                return first_num * second_num
-            except ValueError:
-                print('Enter numbers for the operands...')
-        if(x == '/'):
-            index = expression.index(x)
-            try:
-                first_num = float(expression[0:index])
-                second_num = float(expression[index+1:])
-                return first_num / second_num
-            except ValueError:
-                print('Enter numbers for the operands...')
-        if(x == '**'):
-            index = expression.index(x)
-            try:
-                first_num = float(expression[0:index])
-                second_num = float(expression[index+1:])
-                return first_num ** second_num
-            except ValueError:
-                print('Enter numbers for the operands...')
-print(calc())
+expression = input('Enter a mathematical expression...\nValid operators:\n(+)\n(-)\n(*)\n(/)\n(**)\n')
+for x in expression:
+    if(x == '+'):
+        index = expression.index(x)
+        try:
+            first_num = float(expression[0:index])
+            second_num = float(expression[index+1:])
+            print(first_num + second_num)
+        except ValueError:
+            print('Enter numbers for the operands...')
+    if(x == '-'):
+        index = expression.index(x)
+        try:
+            first_num = float(expression[0:index])
+            second_num = float(expression[index+1:])
+            print(first_num - second_num)
+        except ValueError:
+            print('Enter numbers for the operands...')
+    if(x == '*'):
+        index = expression.index(x)
+        try:
+            first_num = float(expression[0:index])
+            second_num = float(expression[index+1:])
+            print(first_num * second_num)
+        except ValueError:
+            print('Enter numbers for the operands...')
+    if(x == '/'):
+        index = expression.index(x)
+        try:
+            first_num = float(expression[0:index])
+            second_num = float(expression[index+1:])
+            print(first_num / second_num)
+        except ValueError:
+            print('Enter numbers for the operands...')
+    if(x == '**'):
+        index = expression.index(x)
+        try:
+            first_num = float(expression[0:index])
+            second_num = float(expression[index+1:])
+            print(first_num ** second_num)
+        except ValueError:
+            print('Enter numbers for the operands...')
