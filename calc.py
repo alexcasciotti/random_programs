485120357b325588a3e04f4a26be1b012bb10da0
Alex Casciotti <alexc2048@gmail.com>
2022-09-19 21:41:07 -0400

    updated calc.py

--- calc.py
+++ calc.py
@@ -1,29 +1,33 @@
 def calc(x, y, op):
     if(op == '+'):
         return x + y
-    if(op == '-'):
+    elif(op == '-'):
         return x - y
-    if(op == '*'):
+    elif(op == '*'):
         return x * y
-    if(op == '/'):
+    elif(op == '/'):
         return x / y
-    if(op == '**'):
+    elif(op == '**'):
         return x ** y
+    else:
+        return 'Invalid input.'
         
 exit = 0
 saved_outputs = []
 while(exit != 1):
     try:
         x = float(input('X : '))
-        y = float(input('Y : '))
+        y = float(input('Y : '))                                           
         op = input('Operation?\n(+)\n(-)\n(*)\n(/)\n(**)\n')
         result = calc(x, y, op)
         saved_outputs.append(result)
         print(f'{x} {op} {y} = {result}')
-        view_saved = input('View saved outputs?(Y or N)')
+        view_saved = input('View saved outputs?(Y or N)\n')
         if(view_saved.upper() == 'Y'):
-            for x in saved_outputs:
-                print(x)
+            i = len(saved_outputs) - 1
+            while(i >= 0):
+                print(saved_outputs[i])
+                i -= 1
         exit = input('Exit?(Y or N)\n')
         if(exit.upper() == 'Y'):
             exit = 1
