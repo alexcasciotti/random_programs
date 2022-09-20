4b41b7f4cc0ed63a76ef81650623f746a28c5adf
Alex Casciotti <alexc2048@gmail.com>
2022-09-19 21:32:47 -0400

    updated calc.py

--- calc.py
+++ calc.py
@@ -1,5 +1,3 @@
-import data_structures as ds
-
 def calc(x, y, op):
     if(op == '+'):
         return x + y
@@ -13,19 +11,19 @@ def calc(x, y, op):
         return x ** y
         
 exit = 0
-saved_outputs = ds.myStack()
+saved_outputs = []
 while(exit != 1):
     try:
         x = float(input('X : '))
         y = float(input('Y : '))
         op = input('Operation?\n(+)\n(-)\n(*)\n(/)\n(**)\n')
         result = calc(x, y, op)
-        saved_outputs.push(result)
+        saved_outputs.append(result)
         print(f'{x} {op} {y} = {result}')
         view_saved = input('View saved outputs?(Y or N)')
         if(view_saved.upper() == 'Y'):
-            while(saved_outputs.isEmpty() == False):
-                print(saved_outputs.pop())
+            for x in saved_outputs:
+                print(x)
         exit = input('Exit?(Y or N)\n')
         if(exit.upper() == 'Y'):
             exit = 1
